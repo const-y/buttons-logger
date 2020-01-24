a31f3263339f36e7d97f9a7ea51bf1fe74bb82f1
Konstantin Yarish <pm-mc@mail.ru>
2020-01-24 21:52:06 +0300

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const buttons = [...Array(3)];
+
 function App() {
+  const makeBtnHandler = index => () => {
+    alert('Кнопка ' + index);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="buttons">
+          {
+            buttons.map((btn, index) => (
+              <button onClick={makeBtnHandler(index)}>Кнопка {index}</button>
+            ))
+          }
+        </div>
+      </div>
+
     </div>
   );
 }
